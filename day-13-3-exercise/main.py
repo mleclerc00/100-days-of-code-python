1ae656fdb69f9d2c43a3e2fd638040f4728b7f4c
mleclerc <mleclerc@loc.gov>
2023-08-15 12:29:48 -0700

    fix 13-3 exercise

--- day-13-3-exercise/main.py
+++ day-13-3-exercise/main.py
@@ -1,10 +1,12 @@
 for number in range(1, 101):
     # use and instead of or since it needs to be evenly divisible by 3 and 5
+    # also fix logic to use elif statements instead of printing the number
+    # as a else to the first if
     if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
-    if number % 3 == 0:
+    elif number % 3 == 0:
         print("Fizz")
-    if number % 5 == 0:
+    elif number % 5 == 0:
         print("Buzz")
     else:
-        print([number])
+        print(number)
